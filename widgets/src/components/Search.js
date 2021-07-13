d488ed6b41d8cd1d5360a375e009f891544d8d1d
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-07-13 14:02:48 +0300

    Added axios

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,7 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Search = () => {
   const [term, setTerm] = useState("");
+
+  console.log("I RUN WITH EVERY RENDER!");
+  // Second parameter on useEffect:
+  // 1. [] == run initial render,
+  // 2. nothing == 1. + run after every render
+  // 3. [data] == 1. + run after every render if data has changed
+  useEffect(() => {
+    console.log("I ONLY RUN ALSO");
+  }, []);
 
   return (
     <div>
