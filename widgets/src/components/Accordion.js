9bdb97056ab3eb1d416f0460acb5fc6a5829bd74
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-07-13 12:34:57 +0300

    Set state according to the onClick events

--- widgets/src/components/Accordion.js
+++ widgets/src/components/Accordion.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 const Accordion = ({ items }) => {
+  // default value null will be used on when first initialized the state
   const [activeIndex, setActiveIndex] = useState(null);
 
   const onTitleClick = (index) => {
@@ -8,24 +9,20 @@ const Accordion = ({ items }) => {
   };
 
   const renderedItems = items.map((item, index) => {
+    const active = index === activeIndex ? "active" : "";
     return (
       <React.Fragment key={item.title}>
-        <div className='title active' onClick={() => onTitleClick(index)}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index)}>
           <i className='dropdown icon'></i>
           {item.title}
         </div>
-        <div className='content active'>
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
-  return (
-    <div className='ui styled accordion'>
-      {renderedItems}
-      <h1>{activeIndex}</h1>
-    </div>
-  );
+  return <div className='ui styled accordion'>{renderedItems}</div>;
 };
 
 export default Accordion;
