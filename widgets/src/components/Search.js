3593a0352971ad7d8247a2548e683302c22a961b
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-07-16 12:13:01 +0300

    Fixed a bug of two network request to one by splitting the useEffect

--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const Search = () => {
   const [term, setTerm] = useState("programming");
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
   const [results, setResults] = useState([]);
 
   // Second parameter on useEffect:
@@ -10,8 +11,16 @@ const Search = () => {
   // 2. nothing == 1. + run after every render
   // 3. [data] == 1. + run after every render if data has changed
   useEffect(() => {
-    console.log("Initial render or term was changed");
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 1000);
 
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
+
+  useEffect(() => {
     const search = async () => {
       const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
         params: {
@@ -19,26 +28,14 @@ const Search = () => {
           list: "search",
           origin: "*",
           format: "json",
-          srsearch: term,
+          srsearch: debouncedTerm,
         },
       });
       setResults(data.query.search);
     };
 
-    if (term && !results.length) {
-      search();
-    } else {
-      const timeoutId = setTimeout(() => {
-        if (term) {
-          search();
-        }
-      }, 1000);
-
-      return () => {
-        clearTimeout(timeoutId);
-      };
-    }
-  }, [term]);
+    search();
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
     return (
