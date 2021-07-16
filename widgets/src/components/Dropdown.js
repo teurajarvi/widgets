7b23fd767e451adf741fb066aa36640cd9c1ec54
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-07-16 14:02:30 +0300

    Added a useRef to check whether the DOM component clicked (event) or not

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,12 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const Dropdow = ({ options, selected, onSelectedChange }) => {
   const [open, setOpen] = useState(false);
+  const ref = useRef();
 
   useEffect(() => {
     document.body.addEventListener(
       "click",
-      () => {
+      (event) => {
+        if (ref.current.contains(event.target)) {
+          return;
+        }
         setOpen(false);
       },
       { capture: true }
@@ -29,7 +33,7 @@ const Dropdow = ({ options, selected, onSelectedChange }) => {
   });
 
   return (
-    <div className='ui form'>
+    <div ref={ref} className='ui form'>
       <div className='field'>
         <label className='label'>Select a Color</label>
         <div
