801114d824aabe44b4078d13a932420bb212a4bd
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-07-16 12:50:57 +0300

    Added a new state for handling the user selection

--- widgets/src/components/Dropdown.js
+++ widgets/src/components/Dropdown.js
@@ -1,9 +1,12 @@
 import React from "react";
 
-const Dropdow = ({ options }) => {
+const Dropdow = ({ options, selected, onSelectedChange }) => {
   const renderedOptions = options.map((option) => {
     return (
-      <div key={option.value} className='item'>
+      <div
+        key={option.value}
+        className='item'
+        onClick={() => onSelectedChange(option)}>
         {option.label}
       </div>
     );
@@ -14,7 +17,7 @@ const Dropdow = ({ options }) => {
         <label className='label'>Select a Color</label>
         <div className='ui selection dropdown visible active'>
           <i className='dropdown icon'></i>
-          <div className='text'>Select Color</div>
+          <div className='text'>{selected.label}</div>
           <div className='menu visible transition'>{renderedOptions}</div>
         </div>
       </div>
