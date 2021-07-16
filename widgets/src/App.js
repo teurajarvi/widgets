31eccbea99352ab60865dbc4cec9850f9f0cd951
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-07-16 12:26:33 +0300

    Added options array and deliver those as props to a Dropdown component

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -18,10 +18,25 @@ const items = [
   },
 ];
 
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "A Shade of Blue",
+    value: "blue",
+  },
+];
+
 export default () => {
   return (
     <div>
-      <Dropdow />
+      <Dropdow options={options} />
     </div>
   );
 };
