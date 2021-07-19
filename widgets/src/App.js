901cc86d2e7429b5790e4f07eeaa67bc26eeab88
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-07-19 15:40:34 +0300

    Added a new component: Header.js

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -4,6 +4,7 @@ import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 import Translate from "./components/Translate";
 import Route from "./components/Route";
+import Header from "./components/Header";
 
 const items = [
   {
@@ -41,6 +42,7 @@ export default () => {
 
   return (
     <div>
+      <Header />
       <Route path='/'>
         <Accordion items={items} />
       </Route>
