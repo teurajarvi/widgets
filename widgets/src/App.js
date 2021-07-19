95b088a27a2f4ead8abba319d25e2c0de303ff2a
Jari-Pekka Teurajärvi <teurajarvi@hotmail.com>
2021-07-19 16:15:16 +0300

    Added proper navigation using the new component Link and Route and Header

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -36,7 +36,7 @@ const options = [
   },
 ];
 
-export default () => {
+const App = () => {
   const [selected, setSelected] = useState(options[0]);
   const [showDropdown, setShowDropdown] = useState(true);
 
@@ -77,3 +77,5 @@ export default () => {
     </div>
   );
 };
+
+export default App;
